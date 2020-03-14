cf3d21c7f476b44bbc60318eaa657cba96a83dfb
Diiar <aitmarowd@gmail.com>
2020-03-14 13:02:11 +0600

    ЭТАП 4. Добавил поле "access" - Общий доступ.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
+DEFAULT_PROJECT_STATUS = 'common'
+PROJECT_STATUS_CHOICES = [(DEFAULT_PROJECT_STATUS, 'Общий'), ('hide', 'Скрытый'), ('private', 'Приватный')]
+
 # Create your models here.
 class File(models.Model):
     file = models.FileField(null=False, blank=False, upload_to='user_files', verbose_name='Файл')
@@ -9,3 +13,4 @@ class File(models.Model):
                                    verbose_name='Автор объявления')
     created_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     downloaded = models.IntegerField(default=0, verbose_name='Количество скачиваний')
+    access = models.CharField(choices=PROJECT_STATUS_CHOICES, default=DEFAULT_PROJECT_STATUS, verbose_name='Доступ', max_length=20)
