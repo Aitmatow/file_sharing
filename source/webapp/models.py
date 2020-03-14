abd1a941de4941f717b480b0203c88e3dd15e739
Diiar <aitmarowd@gmail.com>
2020-03-14 10:02:30 +0600

    1. Добавил модель для файла.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class File(models.Model):
+    file = models.FileField(null=False, blank=False, upload_to='user_files', verbose_name='Файл')
+    description = models.CharField(max_length=120, verbose_name='Описание файла', null=False, blank=False)
+    created_by = models.ForeignKey(User, related_name='files', on_delete=models.CASCADE, null=True, blank=True,
+                                   verbose_name='Автор объявления')
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
